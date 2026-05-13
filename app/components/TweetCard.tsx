d70080e17020517bf2357fb56123e9509ad325ca
Albsbz <best.shop.bot@gmail.com>
2026-05-13 13:35:01 +0200

    add tags to tweet card

--- app/components/TweetCard.tsx
+++ app/components/TweetCard.tsx
@@ -10,16 +10,23 @@ function TweetCard({ tweet }: { tweet: PostEntity }) {
     <article className="Card p-4 border rounded shadow">
       <h2 className="text-lg font-bold">{tweet.title}</h2>
       <p>{tweet.body}</p>
+      <div className="tags flex items-center space-x-2 mt-2">
+        {tweet.tags?.map((tag, index) => (
+          <span key={index} className="tag color-gray-500 text-sm bg-gray-800 text-white px-2 py-1 rounded">
+            #{tag.body}
+          </span>
+        ))}
+      </div>
       <div className="mt-2 flex space-x-4 text-sm text-gray-600">
-        <div className="grow flex items-center">
+        <div className="grow flex items-center justify-center">
           <LikeButton tweetId={tweet.id} type="like" />
           <div className="ml-1">{tweet.reactions.likes}</div>
         </div>
-        <div className="grow flex items-center">
+        <div className="grow flex items-center justify-center">
           <LikeButton tweetId={tweet.id} type="dislike" />
           <div className="ml-1">{tweet.reactions.dislikes}</div>
         </div>
-        <div className="grow flex items-center">
+        <div className="grow flex items-center justify-center">
           <EyeIcon className="w-5" />
           <div className="ml-1">{tweet.views}</div>
         </div>
