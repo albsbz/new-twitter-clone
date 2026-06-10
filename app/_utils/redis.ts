39618874912cdafd58aa6e6ad9385410c995a349
Albsbz <best.shop.bot@gmail.com>
2026-06-10 20:34:49 +0200

    fix deploy

--- app/_utils/redis.ts
+++ app/_utils/redis.ts
@@ -1,5 +1,14 @@
 import Redis from "ioredis";
 import { getServerEnv } from "../lib/env";
 
-const redis = new Redis(getServerEnv().REDIS_URL);
+const redis = new Redis(getServerEnv().REDIS_URL, {
+  lazyConnect: true,
+  enableOfflineQueue: false,
+  maxRetriesPerRequest: 0,
+});
+
+redis.on("error", (error) => {
+  console.error("Redis error:", error.message);
+});
+
 export default redis;
