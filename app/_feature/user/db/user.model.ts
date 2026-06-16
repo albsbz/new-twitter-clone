7fd31838823fd2faf7ea359a932e71237216c658
Albsbz <best.shop.bot@gmail.com>
2026-06-16 18:36:46 +0200

    fix index

--- app/_feature/user/db/user.model.ts
+++ app/_feature/user/db/user.model.ts
@@ -5,7 +5,14 @@ const { Schema } = mongoose;
 const UserSchema = new Schema<UserEntity>({
   email: String,
   password: String,
-  name: { type: String, unique: true },
+  name: {
+    type: String,
+    trim: true,
+    index: {
+      unique: true,
+      partialFilterExpression: { name: { $type: "string" } },
+    },
+  },
   isVerified: Boolean,
   verificationEmailSendAt: { type: Date, default: null },
   createdAt: { type: Date, default: Date.now },
