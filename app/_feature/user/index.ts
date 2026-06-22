7044e3c7154079e401d066be9dbd1f1475a585af
Albsbz <best.shop.bot@gmail.com>
2026-06-22 20:27:44 +0200

    fix build errors

--- app/_feature/user/index.ts
+++ app/_feature/user/index.ts
@@ -1,9 +1,11 @@
 import AuthController from "../auth/auth.controller";
 import UserService from "../user/user.service";
 import UserController from "./user.controller";
+import RefreshTokenService from "../auth/refreshToken.service";
 
 const userService = new UserService();
-const authController = new AuthController({ userService });
+const refreshTokenService = new RefreshTokenService();
+const authController = new AuthController({ userService, refreshTokenService });
 const userController = new UserController({
   authController,
   userService,
